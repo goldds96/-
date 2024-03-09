6b518255129b878a644cc27a202b914f60752acf
goldds96 <86287862+goldds96@users.noreply.github.com>
2024-03-09 23:19:20 +0900

    Create find_binary.py

---
+++ extract_features/find_binary.py
@@ -0,0 +1,23 @@
+import subprocess as sp
+
+def find_binares(path):
+  """
+  Gets a list of possible binaries within a firmware sample.
+
+  [Param]
+  path : firmware image path
+  
+  [Return]
+  list : a list of binaries
+  """
+
+  cmd = f"find '{fw_path}' -executable -type f -exec file {{}} \; | " \
+          f"grep -iv image | grep -iv text | awk -F':' '{{print $1}}'"
+  p = sp.Popen(cmd, stdout=sp.PIPE, stderr=sp.PIPE, shell=True)
+  o, e = p.communicate()
+  if o:
+    # changed to o.decode() for python3
+    return o.decode().split('\n'
+
+  # Null
+  return []
